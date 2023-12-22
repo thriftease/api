e72933ca291e1d1001ddd12c51d6e073843af602
noamolat-fs <noamolat@fullscale.io>
2023-12-22 22:09:24 +0800

    Add TestAuthSignUpMutation

--- authentication/tests.py
+++ authentication/tests.py
@@ -3,6 +3,8 @@ from typing import Any
 
 from authentication.gql import AuthSchema
 from thriftease_api.tests import TestGraphQL
+from users.models import User
+from utils import ObjectDict
 
 
 class TestAuth(TestGraphQL, AuthSchema):
@@ -11,3 +13,23 @@ class TestAuth(TestGraphQL, AuthSchema):
         response = gql(cls.mutation(cls.authSignIn(email, password)).render())
         content = json.loads(response.content)
         return dict(HTTP_AUTHORIZATION=f'JWT {content["data"]["authSignIn"]["token"]}')
+
+    @classmethod
+    def sign_up(cls, gql: Any, **kwargs):
+        response = gql(cls.mutation(cls.authSignUp(**kwargs)).render())
+        content = json.loads(response.content)
+        return content["data"]["authSignUp"]["data"]
+
+
+class TestAuthSignUpMutation(TestAuth):
+    def test(self, gql: Any):
+        props = ObjectDict(
+            email="test@email.com",
+            password="@Password1234",
+            given_name="Test",
+            family_name="User",
+        )
+        self.sign_up(gql, **props)
+
+        # created
+        assert User.objects.filter(email=props.email).first()

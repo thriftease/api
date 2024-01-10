4c1369c1600c23eb807456e770fe7b032980e16c
noamolat-fs <noamolat@fullscale.io>
2024-01-10 19:37:15 +0800

    Add auth query for existing user check

--- authentication/schemas.py
+++ authentication/schemas.py
@@ -1,8 +1,9 @@
-from graphene import Field, ObjectType
+from graphene import Boolean, Field, NonNull, ObjectType, String
 from graphql_jwt import ObtainJSONWebToken, Verify
 from graphql_jwt.decorators import ensure_token
 from graphql_jwt.utils import get_payload, get_user_by_payload
 
+from users.models import User
 from users.schemas import CreateUserMutation, UserType
 
 
@@ -22,6 +23,18 @@ class AuthVerifyMutationPayload(Verify):
     def verify(cls, root, info, token, **kwargs):
         payload = get_payload(token, info.context)
         return cls(payload=payload, user=get_user_by_payload(payload))
+
+
+class AuthQuery(ObjectType):
+    auth_existing = Field(Boolean, email=NonNull(String))
+
+    @staticmethod
+    def resolve_auth_existing(root, info, email: str):
+        try:
+            User.objects.get(email=email)
+            return True
+        except Exception:
+            return False
 
 
 class AuthMutation(ObjectType):
--- thriftease_api/schemas.py
+++ thriftease_api/schemas.py
@@ -1,7 +1,7 @@
 import graphene
 
 from accounts.schemas import AccountMutation, AccountQuery
-from authentication.schemas import AuthMutation
+from authentication.schemas import AuthMutation, AuthQuery
 from currencies.schemas import CurrencyMutation, CurrencyQuery
 from tags.schemas import TagMutation, TagQuery
 from thriftease_api import settings
@@ -11,6 +11,7 @@ from users.schemas import UserMutation, UserQuery
 
 class Query(
     UserQuery,
+    AuthQuery,
     CurrencyQuery,
     AccountQuery,
     TransactionQuery,
